86e38fb17a2c231915a34fe970f71dfeba88070f
urnice1111 <emilianogarram2910@gmail.com>
2025-05-18 20:33:13 -0600

    Initial Commit

---
+++ Video_realidad_aumentadaTests/Video_realidad_aumentadaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_realidad_aumentadaTests.swift
+//  Video_realidad_aumentadaTests
+//
+//  Created by UwU on 18/05/25.
+//
+
+import Testing
+@testable import Video_realidad_aumentada
+
+struct Video_realidad_aumentadaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
